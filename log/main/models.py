fd1bef0a720db6b7ff9218a647e8153d6adf6cd9
Dmitry7007 <99890690+Dmitry7007@users.noreply.github.com>
2023-11-16 01:21:24 +0700

    Update models.py
    16.11

--- log/main/models.py
+++ log/main/models.py
@@ -4,3 +4,13 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+class Comment(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
